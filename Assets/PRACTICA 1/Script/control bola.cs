3c8b686e5fc147a5cff8110c8f8c3df99d698160
Motzz22 <metzliisabelgp@gmail.com>
2025-10-01 10:39:23 -0600

    Add files via upload
    se agregaron pinos animados, camara con movimiento todo bien

--- Assets/PRACTICA 1/Script/control bola.cs
+++ Assets/PRACTICA 1/Script/control bola.cs
@@ -4,22 +4,63 @@ using UnityEngine;
 
 public class controlbola : MonoBehaviour
 {
+    public Transform camaraPrincipal;
+
     public Rigidbody rb;
+
+    public float velocidadDeApuntado = 5f;
+    public float limiteIzquierda = -2f;
+    public float limiteDerecha = 2f;
+
     public float fuerzaDeLanzamiento = 1000f;
+
+    private bool haSidolansada = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+     
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (haSidolansada == false)
         {
-            rb.AddForce(Vector3.forward * fuerzaDeLanzamiento);
-          
+            Apuntar();
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Lanzar();
+
+            }
         }
     }
-}
+
+    void Apuntar()
+    {
+        float inputHorizontal = Input.GetAxis("Horizontal");
+
+        transform.Translate (Vector3.right *  inputHorizontal *  velocidadDeApuntado * Time.deltaTime);
+
+        Vector3 posicionActual = transform.position;
+
+        //TransformPotition me permite sabe la posicion actual de la bola
+
+        posicionActual.x=Mathf.Clamp(posicionActual.x, limiteIzquierda,limiteDerecha);
+
+        transform.position = posicionActual;
+    }
+
+    void Lanzar()
+    {
+        haSidolansada = true;
+        rb.AddForce(Vector3.forward * fuerzaDeLanzamiento);
+
+        if (camaraPrincipal != null)
+        {
+            camaraPrincipal.SetParent(transform);
+        }
+
+    }
+
+}//YOU SHALL NOT PASS!!!
